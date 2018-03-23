78899a31fb73030e14faa4da99024f1888fa7b04
Tim <timchenyx@gmail.com>
2018-03-24 00:15:00 +0800

    small fix

--- tripvote/Program.cs
+++ tripvote/Program.cs
@@ -14,21 +14,18 @@ namespace tripvote
                 .Select(i => i.Split(',').Last().ToUpper()) //assume last column is the vote
                 .Where(i => i != "NO")  //ignore vote = "NO"
                 );
-
             int round = 1;
             while (lines.Count > 0)
             {
+                Console.WriteLine("Round " + round++);
                 var total = lines.Count();
                 var t = lines.Where(i => i.Length > 0)  //ignore empty votes
                     .Select(i => i[0])                  //get first option of each vote
-                    .GroupBy(i => i, (i, j) => new { vote = i, count = j.Count(), percent = j.Count() * 100 / total })  //groupby first option
-                    .OrderBy(i => i.count);             //order by count
-
-                Console.WriteLine("Round " + round++);
-                foreach (var i in t)
-                    Console.WriteLine(i.vote + ":" + i.count + " " + i.percent + "%");
-
-                lines = lines.Select(i => i.Replace(t.First().vote.ToString(), "")) //remove least popular option for this round
+                    .GroupBy(i => i, (i, j) => new { option = i, count = j.Count(), percent = j.Count() * 100 / total })  //groupby first option
+                    .OrderBy(i => i.count)              //order by count
+                    .ToList();
+                foreach (var i in t) Console.WriteLine(i.option + ":" + i.count + " " + i.percent + "%");
+                lines = lines.Select(i => i.Replace(t.First().option.ToString(), "")) //remove least popular option for this round
                     .Where(i => i.Length > 0)
                     .ToList();
                 Console.ReadLine();
