3cecaff601867f8cedfaa18017eba90fb74f296c
Tim <timchenyx@gmail.com>
2018-03-23 23:57:19 +0800

    Add some comments

--- tripvote/Program.cs
+++ tripvote/Program.cs
@@ -11,24 +11,24 @@ namespace tripvote
         {
             List<string> lines = new List<string>(
                 File.ReadAllLines(@"C:\scratch\Source\Repos\tripvote\vote.csv")
-                .Select(i => i.Split(',').Last().ToUpper())
-                .Where(i => i != "NO")
+                .Select(i => i.Split(',').Last().ToUpper()) //assume last column is the vote
+                .Where(i => i != "NO")  //ignore vote = "NO"
                 );
 
             int round = 1;
             while (lines.Count > 0)
             {
                 var total = lines.Count();
-                var t = lines.Where(i => i.Length > 0)
-                    .Select(i => i[0])
-                    .GroupBy(i => i, (i, j) => new { vote = i, count = j.Count(), percent = j.Count() * 100 / total })
-                    .OrderBy(i => i.count);
+                var t = lines.Where(i => i.Length > 0)  //ignore empty votes
+                    .Select(i => i[0])                  //get first option of each vote
+                    .GroupBy(i => i, (i, j) => new { vote = i, count = j.Count(), percent = j.Count() * 100 / total })  //groupby first option
+                    .OrderBy(i => i.count);             //order by count
 
                 Console.WriteLine("Round " + round++);
                 foreach (var i in t)
                     Console.WriteLine(i.vote + ":" + i.count + " " + i.percent + "%");
 
-                lines = lines.Select(i => i.Replace(t.First().vote.ToString(), ""))
+                lines = lines.Select(i => i.Replace(t.First().vote.ToString(), "")) //remove least popular option for this round
                     .Where(i => i.Length > 0)
                     .ToList();
                 Console.ReadLine();
